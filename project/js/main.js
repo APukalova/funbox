ee2f5edc0da57dd15769b2b29c3d1f4b91a2657d
PukalovaA <anastasia.pukalova@gmail.com>
2018-03-20 15:28:44 +0300

    first version

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
 
+    var $productItem = $('.product-list__item');
+
+    $productItem.each(function() {
+
+        $(this).on('click', function(e) {
+            var $this = $(this);
+
+            if (!$(e.target).closest('.product-slogan').length && !$this.hasClass('_disabled')) {
+                setCardCondition($this);
+            }
+        })
+
+        $(this).find('.product-slogan .link').on('click', function(e) {
+            var $this = $(this).closest('.product-list__item');
+
+            e.preventDefault();
+            setCardCondition($this);
+        })
+
+        $(this).mouseleave( function(e){
+            if (!$(e.target).closest('.product-slogan').length) {
+                if ($(this).hasClass('_active') && !$(this).hasClass('_selected')) {
+                    $(this).addClass('_selected');
+                }
+            }
+        })
+    })
+
+    function setCardCondition(e) {
+        e.toggleClass('_active');
+
+        if (!e.hasClass('_active')) {
+            e.removeClass('_selected');
+        }
+    }
+
 });
